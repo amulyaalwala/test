9a5d5ed7f0d8a12d9f7eff85fc3aa24ddfe80df1
amulyaalwala <aalwala@usf.edu>
2021-05-27 21:34:24 -0400

    commit test

---
+++ AAGITTEST/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AAGITTEST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("test git");
+        }
+    }
+}
